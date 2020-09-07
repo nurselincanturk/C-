33b272a3506caa9f9d95db4aa4d5d6c4f9f97492
nurselincanturk <nurselincanturk@outlook.com>
2020-09-07 14:57:02 +0300

    Add files via upload

---
+++ YumurtaSayProject/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace ShapeChecker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.loadDemoImage1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadDemoImage2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadDemoImage3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadDemoImage4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(136, 39);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(426, 295);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip.Size = new System.Drawing.Size(699, 28);
+            this.menuStrip.TabIndex = 0;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.loadDemoImage1ToolStripMenuItem,
+            this.loadDemoImage2ToolStripMenuItem,
+            this.loadDemoImage3ToolStripMenuItem,
+            this.loadDemoImage4ToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(218, 26);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(215, 6);
+            // 
+            // loadDemoImage1ToolStripMenuItem
+            // 
+            this.loadDemoImage1ToolStripMenuItem.Name = "loadDemoImage1ToolStripMenuItem";
+            this.loadDemoImage1ToolStripMenuItem.Size = new System.Drawing.Size(218, 26);
+            this.loadDemoImage1ToolStripMenuItem.Text = "Load demo image &1";
+            this.loadDemoImage1ToolStripMenuItem.Click += new System.EventHandler(this.loadDemoImage1ToolStripMenuItem_Click);
+            // 
+            // loadDemoImage2ToolStripMenuItem
+            // 
+            this.loadDemoImage2ToolStripMenuItem.Name = "loadDemoImage2ToolStripMenuItem";
+            this.loadDemoImage2ToolStripMenuItem.Size = new System.Drawing.Size(218, 26);
+            this.loadDemoImage2ToolStripMenuItem.Text = "Load demo image &2";
+            this.loadDemoImage2ToolStripMenuItem.Click += new System.EventHandler(this.loadDemoImage2ToolStripMenuItem_Click);
+            // 
+            // loadDemoImage3ToolStripMenuItem
+            // 
+            this.loadDemoImage3ToolStripMenuItem.Name = "loadDemoImage3ToolStripMenuItem";
+            this.loadDemoImage3ToolStripMenuItem.Size = new System.Drawing.Size(218, 26);
+            this.loadDemoImage3ToolStripMenuItem.Text = "Load demo image &3";
+            this.loadDemoImage3ToolStripMenuItem.Click += new System.EventHandler(this.loadDemoImage3ToolStripMenuItem_Click);
+            // 
+            // loadDemoImage4ToolStripMenuItem
+            // 
+            this.loadDemoImage4ToolStripMenuItem.Name = "loadDemoImage4ToolStripMenuItem";
+            this.loadDemoImage4ToolStripMenuItem.Size = new System.Drawing.Size(218, 26);
+            this.loadDemoImage4ToolStripMenuItem.Text = "Load demo image &4";
+            this.loadDemoImage4ToolStripMenuItem.Click += new System.EventHandler(this.loadDemoImage4ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(215, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(218, 26);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(13, 1);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(669, 55);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Legend";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(243, 19);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 9;
+            this.toolTip.SetToolTip(this.label9, "Equilateral, Isosceles, Rectangled or Rectangled Isosceles Triangle");
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Image files (*.jpg,*.png,*.tif,*.bmp,*.gif)|*.jpg;*.png;*.tif;*.bmp;*.gif|JPG fil" +
+    "es (*.jpg)|*.jpg|PNG files (*.png)|*.png|TIF files (*.tif)|*.tif|BMP files (*.bm" +
+    "p)|*.bmp|GIF files (*.gif)|*.gif";
+            this.openFileDialog.Title = "Open image file";
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.pictureBox);
+            this.splitContainer.Panel2.Resize += new System.EventHandler(this.splitContainer_Panel2_Resize);
+            this.splitContainer.Size = new System.Drawing.Size(699, 442);
+            this.splitContainer.SplitterDistance = 62;
+            this.splitContainer.SplitterWidth = 5;
+            this.splitContainer.TabIndex = 1;
+            this.splitContainer.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(699, 470);
+            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(714, 506);
+            this.Name = "MainForm";
+            this.Text = "Simple Shape Checker";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem loadDemoImage1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadDemoImage2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadDemoImage3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadDemoImage4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ YumurtaSayProject/MainForm.cs
@@ -0,0 +1,214 @@
+﻿
+// http://www.aforgenet.com/framework/
+
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Reflection;
+
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+
+namespace ShapeChecker
+{
+    public partial class MainForm : Form
+    {
+        public MainForm( )
+        {
+            InitializeComponent( );
+        }
+
+        private void exitToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            this.Close( );
+        }
+
+        private void MainForm_Load( object sender, EventArgs e )
+        {
+            LoadDemo( "coins.jpg" );
+        }
+
+        private void openToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            if ( openFileDialog.ShowDialog( ) == DialogResult.OK )
+            {
+                try
+                {
+                    ProcessImage( (Bitmap) Bitmap.FromFile( openFileDialog.FileName ) );
+                }
+                catch
+                {
+                    MessageBox.Show( "Failed loading selected image file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                }
+            }
+        }
+
+        private void loadDemoImage1ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            LoadDemo( "coins.jpg" );
+        }
+
+        private void loadDemoImage2ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            LoadDemo( "demo.png" );
+        }
+
+        private void loadDemoImage3ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            LoadDemo( "demo1.png" );
+        }
+
+        private void loadDemoImage4ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            LoadDemo( "demo2.png" );
+        }
+      
+
+        private void LoadDemo( string embeddedFileName )
+        {
+            Assembly assembly = this.GetType( ).Assembly;
+            Bitmap image = new Bitmap( assembly.GetManifestResourceStream( "ShapeChecker." + embeddedFileName ) );
+            ProcessImage( image );
+        }
+
+        
+        private void ProcessImage( Bitmap bitmap )
+        {
+            int toplamSekilSayisi=0;
+            BitmapData bitmapData = bitmap.LockBits(
+                new Rectangle( 0, 0, bitmap.Width, bitmap.Height ),
+                ImageLockMode.ReadWrite, bitmap.PixelFormat );
+
+            ColorFiltering colorFilter = new ColorFiltering( );
+
+            colorFilter.Red   = new IntRange( 100, 255 );
+            colorFilter.Green = new IntRange( 0, 75 );
+            colorFilter.Blue  = new IntRange( 0, 75 );
+
+            colorFilter.ApplyInPlace( bitmapData );
+
+            BlobCounter blobCounter = new BlobCounter( );
+
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 5;
+            blobCounter.MinWidth = 5;
+
+            blobCounter.ProcessImage( bitmapData );
+            Blob[] blobs = blobCounter.GetObjectsInformation( );
+            bitmap.UnlockBits( bitmapData );
+
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker( );
+
+            Graphics g = Graphics.FromImage( bitmap );
+            Pen yellowPen = new Pen( Color.Yellow, 2 ); 
+           
+            for ( int i = 0, n = blobs.Length; i < n; i++ )
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints( blobs[i] );
+
+                DoublePoint center;
+                double radius;
+
+                if ( shapeChecker.IsCircle( edgePoints, out center, out radius ) )
+                {
+                    g.DrawEllipse( yellowPen,
+                        (float) ( center.X - radius ), (float) ( center.Y - radius ),
+                        (float) ( radius * 2 ), (float) ( radius * 2 ) );
+                    toplamSekilSayisi++;
+                }
+                label9.Text = "boşluk sayısı : " + (toplamSekilSayisi.ToString());
+                
+               
+
+
+            }
+
+            if (toplamSekilSayisi < 5 &&toplamSekilSayisi >= 1)
+            {
+                string message = "Yumurta sayısı azaldı." +
+                    "Sipariş verilsin mi?";
+                string title = "Yumurta Kontrol";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show(message, title, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    MessageBox.Show("Siparişiniz verildi.");
+                }
+                else
+                {
+                }
+            }
+
+
+
+            yellowPen.Dispose( );
+           
+            g.Dispose( );
+
+            Clipboard.SetDataObject( bitmap );
+            pictureBox.Image = bitmap;
+
+            UpdatePictureBoxPosition( );
+        }
+
+        private void splitContainer_Panel2_Resize( object sender, EventArgs e )
+        {
+            UpdatePictureBoxPosition( );
+        }
+
+        private void UpdatePictureBoxPosition( )
+        {
+            int imageWidth;
+            int imageHeight;
+
+            if ( pictureBox.Image == null )
+            {
+                imageWidth  = 320;
+                imageHeight = 240;
+            }
+            else
+            {
+                imageWidth  = pictureBox.Image.Width;
+                imageHeight = pictureBox.Image.Height;
+            }
+
+            Rectangle rc = splitContainer.Panel2.ClientRectangle;
+
+            pictureBox.SuspendLayout( );
+            pictureBox.Location = new Point( ( rc.Width - imageWidth - 2 ) / 2, ( rc.Height - imageHeight - 2 ) / 2 );
+            pictureBox.Size = new Size( imageWidth + 2, imageHeight + 2 );
+            pictureBox.ResumeLayout( );
+        }
+
+        private Point[] ToPointsArray( List<IntPoint> points )
+        {
+            Point[] array = new Point[points.Count];
+
+            for ( int i = 0, n = points.Count; i < n; i++ )
+            {
+                array[i] = new Point( points[i].X, points[i].Y );
+            }
+
+            return array;
+        }
+
+        private void aboutToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            AboutForm form = new AboutForm( );
+
+            form.ShowDialog( );
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
